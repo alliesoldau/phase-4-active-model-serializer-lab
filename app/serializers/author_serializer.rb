e146c1ff6e0a0241c00af301439ceb9dded1e69f
Allie Soldau <alliesoldau@gmail.com>
2023-01-11 14:17:07 -0500

    still failing 2 test

---
+++ app/serializers/author_serializer.rb
@@ -0,0 +1,6 @@
+class AuthorSerializer < ActiveModel::Serializer
+  attributes :name
+  has_one :profile
+  # attributes :username, :email, :bio, :avatar_url
+  has_many :posts, serializer: AuthorPostSerializer
+end
