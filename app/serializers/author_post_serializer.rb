73ac3081583077dae090690cdef3df1150853f89
Allie Soldau <alliesoldau@gmail.com>
2023-01-12 17:07:07 -0500

    commit tree b498b952b693df35f4883fe6ce4d11e2f8f64200

--- app/serializers/author_post_serializer.rb
+++ app/serializers/author_post_serializer.rb
@@ -1,6 +1,6 @@
 class AuthorPostSerializer < ActiveModel::Serializer
   attributes :title, :short_content
-  has_many :tags
+  has_many :post
 
   def short_content
     "#{object.content[0..39]}..."
--- app/serializers/author_serializer.rb
+++ app/serializers/author_serializer.rb
@@ -2,5 +2,5 @@ class AuthorSerializer < ActiveModel::Serializer
   attributes :name
   has_one :profile
   # attributes :username, :email, :bio, :avatar_url
-  has_many :posts, serializer: AuthorPostSerializer
+  has_many :posts
 end
